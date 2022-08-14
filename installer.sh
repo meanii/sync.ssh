7b6201023334e89685d595fc582a2996386de79e
Anil <anilchauhanxda@gmail.com>
2022-08-14 23:33:42 +0530

    created timer daemon service, and installer.sh script for making easier to install sync.ssh! By default the deamon service will run every 30 mins!

---
+++ installer.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#
+# Copyright © 2022 Anil Chauhan <https://github.com/meanii>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+## Installing sync.ssh from the source
+echo "Installing sync.ssh"
+go install .
+
+## Preparing daemon file
+echo "setting up daemon services"
+SYNCSSH_PATH=$(which sync.ssh)
+SEVICE_FILE_BIN=service/sync_bin.service
+SEVICE_FILE=service/sync.service
+TIMER_FILE=service/sync.timer
+
+SYSTEMD_PATH=/lib/systemd/system/
+
+cat $SEVICE_FILE_BIN | sed "s|SYNCSSH_PATH|$SYNCSSH_PATH|g; s|USERNAME|$USER|g;" > $SEVICE_FILE
+
+## Coping Prepared Daemon file to the /lib/systemc/system
+echo "copping demon service files"
+cp $SEVICE_FILE $SYSTEMD_PATH
+cp $TIMER_FILE $SYSTEMD_PATH
+
+## Relaoding, Enabling and Starting the daemon services
+sudo systemctl daemon-reload
+sudo systemctl enable sync
+sudo systemctl enable sync.timer
+
+sudo systemctl start sync
+sudo systemctl start sync.timer
+
+echo "sync.ssh installation has been done!"
+echo "try sync.ssh --help"
